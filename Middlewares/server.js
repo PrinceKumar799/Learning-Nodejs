531c18bae13d9e0427ad2ffc337c4a2e5bd58e6f
PrinceKumar799 <singhprince799@outlook.com>
2023-02-28 21:24:32 +0530

    Middleware completed

--- Middlewares/server.js
+++ Middlewares/server.js
@@ -3,14 +3,27 @@ const app = express();
 const path = require('path');
 const cors = require('cors');
 const {logger} = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
 const PORT = process.env.PORT || 3500;
 
 //custom middleware logger
 app.use(logger);
 
 //cross origin resource sharing
-const whitelist = [];
-app.use(cors())
+const whitelist = ['https://www.google.com','https://127.0.0.1:5500','http://localhost:3500/'];
+const corsOptions = {
+    origin: (origin,callback) => {
+        if(whitelist.indexOf(origin) != -1 || !origin)
+        {
+            callback(null,true);
+        }
+        else{
+            callback(new Error("Not allowed by CORS"));
+        }
+    },
+    optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 app.use(express.urlencoded({extended: false}));
 
@@ -54,7 +67,17 @@ app.get('/hello(.html)?',(req,res,next) => {
 },(req,res)=>{
     res.send('Hello World!');
 })
-app.get('/*',(req,res) => {
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+app.all('*',(req,res) => {
+    res.status(404);
+    if(req.accepts('html'))
+    res.sendFile(path.join(__dirname,'views','404.html'));
+    else if(req.accepts('json'))
+    res.json({error: "404 not found"});
+    else
+    res.type('txt').send("404 Not Found");
 })
+app.use(errorHandler);
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+//
